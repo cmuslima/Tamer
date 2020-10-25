1dccccb9fb9bfc268d081057a393721b5e5e904c
cmuslima <51674886+cmuslima@users.noreply.github.com>
2020-10-25 13:04:18 -0400

    Update Tamer_MountainCar.py

--- Tamer_MountainCar.py
+++ Tamer_MountainCar.py
@@ -1,9 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
+'''
+    This is a demo file to be replaced by the researcher as required.
+    This file is imported by trial.py and trial.py will call:
+    start()
+    step()
+    render()
+    reset()
+    close()
+    These functions are mandatory. This file contains minimum working versions
+    of these functions, adapt as required for individual research goals.
+    '''
 
-# In[ ]:
+import gym
+import time
+import numpy as np
+import itertools
 
 
+#This is the code for tile coding features
+#Rich Sutton tile coding code below
 class IHT:
     "Structure to handle collisions"
     def __init__(self, sizeval):
@@ -75,52 +89,28 @@ def tileswrap (ihtORsize, numtilings, floats, wrapwidths, ints=[], readonly=Fals
     return Tiles
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-import numpy as np
-import itertools
-import matplotlib.pyplot as plt
-import gym
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 class MountainCarTileCoder:
     def __init__(self, iht_size=4096, num_tilings=8, num_tiles=8):
         """
-        Initializes the MountainCar Tile Coder
-       
-        iht_size -- int, the size of the index hash table, typically a power of 2
-        num_tilings -- int, the number of tilings
-        num_tiles -- int, the number of tiles. Here both the width and height of the
-                     tile coder are the same
-
+            Initializes the MountainCar Tile Coder
+            
+            iht_size -- int, the size of the index hash table, typically a power of 2
+            num_tilings -- int, the number of tilings
+            num_tiles -- int, the number of tiles. Here both the width and height of the
+            tile coder are the same
+            """
         self.iht = IHT(iht_size)
         self.num_tilings = num_tilings
         self.num_tiles = num_tiles
-        """
+            
     def get_tiles(self, position, velocity):
         """
-        Takes in a position and velocity from the mountaincar environment
-        and returns a numpy array of active tiles.
-        
-        returns:
-        tiles - np.array, active tiles
-        """
+            Takes in a position and velocity from the mountaincar environment
+            and returns a numpy array of active tiles.
+            
+            returns:
+            tiles - np.array, active tiles
+            """
         # Use the ranges above and self.num_tiles to scale position and velocity to the range [0, 1]
         # then multiply that range with self.num_tiles so it scales from [0, num_tiles]
         minP=-1.2
@@ -133,7 +123,7 @@ class MountainCarTileCoder:
         position_scaled = ((position-minP)/(scaleP))*self.num_tiles
         
         velocity_scaled = ((velocity-minV)/(scaleV))*self.num_tiles
-       
+        
         
         # get the tiles using tc.tiles, with self.iht, self.num_tilings and [scaled position, scaled velocity]
         # nothing to implment here
@@ -142,201 +132,222 @@ class MountainCarTileCoder:
         return np.array(mytiles)
 
 
-# In[ ]:
-
+#this is the tamer agent class
 
 class TamerAgent:
     """
-    Initialization of Tamer Agent. All values are set to None so they can
-    be initialized in the agent_init method.
-    """
+        Initialization of Tamer Agent. All values are set to None so they can
+        be initialized in the agent_init method.
+        """
     def __init__(self):
         self.last_action = None
-        self.last_state = None
-            
         self.previous_tiles = None
-    
+        self.first_state= None
+        self.current_action = None
+        self.current_tiles= None
+        
         self.num_tilings =  8
         self.num_tiles =  8
         self.iht_size =  4096
         self.epsilon = 0.1
-        self.gamma = 1.0 # this is discount
-        self.x = .32
+        self.gamma = 0 # this is discount
+        self.x = .08
         self.alpha =self.x/self.num_tilings  #this is step size
         self.initial_weights =  0.0
         self.num_actions = 3
         self.actions = list(range(self.num_actions))
+        self.time_step=0
+        self.credit_window=[]
+        self.max_credits=20
         
         # We initialize self.w to three times the iht_size. Recall this is because
         # we need to have one set of weights for each action.
         self.w = np.ones((self.num_actions, self.iht_size)) * self.initial_weights
         
-        # We initialize self.mctc to the mountaincar verions of the 
+        # We initialize self.mctc to the mountaincar verions of the
         # tile coder that we created
-        self.mctc = MountainCarTileCoder(iht_size=self.iht_size, 
-                                         num_tilings=self.num_tilings, 
-                                         num_tiles=self.num_tiles)
-
         
+        self.mctc = MountainCarTileCoder(iht_size=self.iht_size,
+                                         num_tilings=self.num_tilings,
+                                         num_tiles=self.num_tiles)
+    
     def argmax(self, q_values):
         """argmax with random tie-breaking
-        Args:
+            Args:
             q_values (Numpy array): the array of action values
-        Returns:
+            Returns:
             action (int): an action with the highest value
-        """
+            """
         top = float("-inf")
         ties = []
-
+        
         for i in range(len(q_values)):
             if q_values[i] > top:
                 top = q_values[i]
                 ties = []
-
+            
             if q_values[i] == top:
                 ties.append(i)
-
-        return np.random.choice(ties)   
         
-    def select_action(self, tiles):
+        return np.random.choice(ties)
+
+    def select_greedy_action(self, tiles):
         """
-        Selects an action using epsilon greedy
-        Args:
-        tiles - np.array, an array of active tiles
-        Returns:
-        (chosen_action, action_value) - (int, float), tuple of the chosen action
-                                        and it's value
-        """
+            Selects an action using greedy
+            Args:
+            tiles - np.array, an array of active tiles
+            Returns:
+            (chosen_action, action_value) - (int, float), tuple of the chosen action
+            and it's value
+            """
         action_values = []
         chosen_action = None
-
+        
         for a in range(self.num_actions):
             action_values.append(np.sum(self.w[a][tiles]))
         # First loop through the weights of each action and populate action_values
         # with the action value for each action and tiles instance
         
-        if np.random.random() < self.epsilon:
-            chosen_action = np.random.choice(self.actions)
-        else:
-            #values = self.q_values[state]
-            chosen_action = self.argmax(action_values)
-        return chosen_action, action_values[chosen_action]
+        chosen_action = self.argmax(action_values)
+        return chosen_action
+    
+    def action_selection(self, state):
+        position, velocity = state
+        active_tiles=self.mctc.get_tiles(position, velocity)
+        current_action= self.select_greedy_action(active_tiles)
+        self.current_action = current_action
+        self.current_tiles = np.copy(active_tiles)
+    
     
     def agent_start(self, state):
         """The first method called when the experiment starts, called after
-        the environment starts.
-        Args:
+            the environment starts.
+            Args:
             state (Numpy array): the state observation from the
-                environment's evn_start function.
-        Returns:
+            environment's evn_start function.
+            Returns:
             The first action the agent takes.
-        """
+            """
         position, velocity = state
-       # print('position', position, 'velocity', velocity)
-
+        # print('position', position, 'velocity', velocity)
+        
         active_tiles=self.mctc.get_tiles(position, velocity)
-       # print('active tiles', active_tiles)
-        #active_tiles=mctc.get_tiles(position, velocity)
-
-
-        current_action, current_action_values= self.select_action(active_tiles)
+        
+        current_action= self.select_greedy_action(active_tiles)
         
         self.last_action = current_action
         self.previous_tiles = np.copy(active_tiles)
-       
-        return self.last_action
+        self.current_tiles= np.copy(active_tiles)
         
-    def agent_step(self, reward, state):
-        """A step taken by the agent.
-        Args:
-            reward (float): the reward received for taking the last action taken
-            state (Numpy array): the state observation from the
-                environment's step based, where the agent ended up after the
-                last step
-        Returns:
-            The action the agent is taking.
-        """
-        # choose the action here
-        position, velocity = state
-        
-        # Use self.tc to set active_tiles using position and velocity
-        # set current_action and action_value to the epsilon greedy chosen action using
-        # the select_action function above with the active tiles
-        
-      #  print('check')
-        active_tiles=self.mctc.get_tiles(position, velocity)
-      #  print('check2')
-        current_action, current_action_values= self.select_action(active_tiles)
-    
-        
-        target = reward + current_action_values
-        
-        self.w[self.last_action][self.previous_tiles]+= self.alpha*(target-np.sum(self.w[self.last_action][self.previous_tiles]))
-        
-        
-        self.last_action = current_action
-        self.previous_tiles = np.copy(active_tiles)
         return self.last_action
     
-    def agent_end(self, reward):
-
-        target = reward 
-        
-        self.w[self.last_action][self.previous_tiles]+= self.alpha*(target-np.sum(self.w[self.last_action][self.previous_tiles]))
-        
-
-
-# In[ ]:
-
-
-import time
-np.random.seed(0)
-
-num_runs = 10
-num_episodes = 1000
-
-all_steps = []
-ENV_NAME= "MountainCar-v0"
-myagent =TamerAgent()
-
-start = time.time()
-env = gym.make(ENV_NAME)
-rewards_per_episode=[]
-all_rewards=[]
-   
-steps_per_episode = []
-for i_episode in range(num_episodes):
-    total_reward=0
-    num_steps=0
-    state = env.reset()
-    action = myagent.agent_start(state) #returns the first action the agent takes. 
-  
     
-    while True:
-        
-        state, reward, done, info = env.step(action) #im actually moving now.
-       # print('next state, reward', state, reward)
-        action = myagent.agent_step(reward, state)
-        total_reward+=reward
+    def update_reward_function(self, reward):
+        if reward == 'good':
+            target = 1
+            for a in range(len(self.credit_window)):
+                self.w[self.credit_window[a][0]][self.credit_window[a][1]]+= self.alpha*(target-np.sum(self.w[self.credit_window[a][0]][self.credit_window[a][1]]))
 
-        if done:
-            myagent.agent_end(reward)
-            print("Episode finished after {} timesteps".format(num_steps+1))
-            steps_per_episode.append(num_steps)
-            rewards_per_episode.append(total_reward)           
-            break
+        elif reward == 'bad':
+            target = -1
+            for a in range(len(self.credit_window)):
+                self.w[self.credit_window[a][0]][self.credit_window[a][1]]+= self.alpha*(target-np.sum(self.w[self.credit_window[a][0]][self.credit_window[a][1]]))
+
+    def credit_assignment(self, action, tiles):
+        
+        if len(self.credit_window)== self.max_credits:
+            self.credit_window.pop(self.max_credits-1)
             
-        num_steps+=1
-    #myagent.epsilon*=.99
-    all_steps.append(np.array(steps_per_episode))
-    all_rewards.append(np.array(rewards_per_episode))
-print("Run time: {}".format(time.time() - start))
-
-             
+        self.credit_window.append((action, tiles))
 
 
-# In[ ]:
+#HIPPOGYM
+
+def start(game:str):
+    '''
+        Starts an OpenAI gym environment.
+        Caller:
+        - Trial.start()
+        Inputs:
+        -   game (Type: str corresponding to allowable gym environments)
+        Returs:
+        - env (Type: OpenAI gym Environment as returned by gym.make())
+        Mandatory
+    '''
+    np.random.seed(0)
+    global myagent 
+    myagent= TamerAgent()
+    env = gym.make(game)
+    
+    return env
+
+def step(env, reward):
+    ##action:int is updated to reward: string
+    '''
+        Takes a game step.
+        Caller:
+        - Trial.take_step()
+        Inputs:
+        - env (Type: OpenAI gym Environment)
+        - reward from the user (Type: string)
+        Returns:
+        - envState (Type: dict containing all information to be recorded for future use)
+        change contents of dict as desired, but return must be type dict.
+    '''
+    
+    
+    if myagent.time_step==0: #I need to call agent_start only once at the start
+        myagent.current_action = myagent.agent_start(myagent.first_state) #this should return a0.
+        reward = 'None'
+    
+    myagent.time_step= -1 #this ensures I don't call the start function again
+    myagent.update_reward_function(reward)
+    myagent.credit_assignment(myagent.current_action, myagent.current_tiles)
+    myagent.last_action= myagent.current_action
+    myagent.previous_tiles= myagent.current_tiles
+
+    state, _ , done, info = env.step(myagent.current_action)
+    envState = {'observation': state, 'done': done }
+    if done:
+        return envState
+    myagent.action_selection(state)
+
+    return envState
+
+def render(env):
+    '''
+        Gets render from gym.
+        Caller:
+        - Trial.get_render()
+        Inputs:
+        - env (Type: OpenAI gym Environment)
+        Returns:
+        - return from env.render('rgb_array') (Type: npArray)
+        must return the unchanged rgb_array
+        '''
+    return env.render('rgb_array')
+
+def reset(env):
+    '''
+        Resets the environment to start new episode.
+        Caller:
+        - Trial.reset()
+        Inputs:
+        - env (Type: OpenAI gym Environment)
+        Returns:
+        No Return
+        '''
+    myagent.first_state = env.reset()
 
 
-
-
+def close(env):
+    '''
+        Closes the environment at the end of the trial.
+        Caller:
+        - Trial.close()
+        Inputs:
+        - env (Type: OpenAI gym Environment)
+        Returns:
+        No Return
+        '''
+    env.close()
