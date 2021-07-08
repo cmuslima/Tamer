db9b11f52794c2a1e6a42d135fb65fd10a44d0a3
cmuslima <51674886+cmuslima@users.noreply.github.com>
2021-07-08 12:01:28 -0400

    Update teacher_student_comparison.py

--- LunarLandar/teacher_student_comparison.py
+++ LunarLandar/teacher_student_comparison.py
@@ -1,5 +1,18 @@
 class comparing_teacher_student:
+    
+    def importance_update(teacher_agent, student_action, state, threshold, budget_used, max_budget, unlimited, time_of_feedback, time_step):
+        action_values = teacher_agent.evaluate_q(state)
+        iv=abs(max(action_values)-min(action_values))
 
+        if iv >= threshold:
+            time_of_feedback.append(time_step)
+            reward, update = comparing_teacher_student.compare_actions_budget(teacher_agent, state, student_action, budget_used, max_budget, unlimited)
+        else:
+            reward = None
+            update = 0 
+
+        return reward, update
+    
     def uniform_update(teacher_agent, student_action, state, uniform_value, budget_used, max_budget, unlimited, total_time_step):
         if total_time_step % uniform_value == 0:
             reward, update = comparing_teacher_student.compare_actions_budget(teacher_agent, state, student_action, budget_used, max_budget, unlimited)
